54584ecd253cda70eeb6a9c3914a7ba43d11a085
Aayush <aayush.shah15@gmail.com>
2024-05-05 20:46:46 -0400

    misc

--- dist/save/index.js
+++ dist/save/index.js
@@ -1076,6 +1076,8 @@ function downloadCacheAxiosMultiPart(archiveLocation, archivePath) {
         finally {
             progressLogger === null || progressLogger === void 0 ? void 0 : progressLogger.stopDisplayTimer(true);
             try {
+                // Sleep for 2 seconds to allow the file to be written to disk.
+                yield new Promise(resolve => setTimeout(resolve, 2000));
                 yield fdesc.close();
             }
             catch (err) {
